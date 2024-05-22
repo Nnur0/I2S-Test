2a5ca52f3852055f90278c23ee95f19505245982
DESKTOP-72VH53T\savel <savelsbergnuro@gmail.com>
2024-05-22 21:25:28 +0200

    add code

--- I2sTest/src/main.cpp
+++ I2sTest/src/main.cpp
@@ -1,18 +1,102 @@
 #include <Arduino.h>
+#include "driver/i2s.h"
+#include "freertos/queue.h"
+#include <cmath>
 
+i2s_port_t i2s_in = I2S_NUM_0; // i2s port number
+i2s_port_t i2s_out = I2S_NUM_1; // i2s port number
+
+static const i2s_config_t i2s_config_in = {
+     .mode = i2s_mode_t(I2S_MODE_SLAVE | I2S_MODE_RX),
+     .sample_rate = 44100 ,
+     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+     .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+     .intr_alloc_flags = 0, // default interrupt priority
+     .dma_buf_count = 8,
+     .dma_buf_len = 64,
+     .use_apll = false
+};
+
+static const i2s_config_t i2s_config_out = {
+     .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_TX), //i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_TX),
+     .sample_rate = 44100,
+     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+     .communication_format = I2S_COMM_FORMAT_STAND_I2S, //i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S | I2S_COMM_FORMAT_STAND_MSB),
+     .intr_alloc_flags = 0, // default interrupt priority
+     .dma_buf_count = 2,
+     .dma_buf_len = 64,
+     .use_apll = true,
+     .tx_desc_auto_clear = true
+};
+
+static const i2s_pin_config_t pin_config = {
+    .bck_io_num = 5,
+    .ws_io_num = 25,
+    .data_out_num = 22,
+    .data_in_num = 26
+};
+
+static const i2s_pin_config_t pin_config2 = {
+    .bck_io_num = 32,
+    .ws_io_num = 33,
+    .data_out_num = 27,
+    .data_in_num = 35
+};
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  //i2s_driver_install(i2s_in, &i2s_config_in, 0, NULL);   //install and start i2s driver
+  int32_t i;
+  i2s_driver_install(i2s_out, &i2s_config_out, 0, NULL);   //install and start i2s driver
+
+  //i2s_set_pin(i2s_in, &pin_config);
+  i2s_set_pin(i2s_out, &pin_config2);
+  Serial.begin(115200);
 }
+
+#define type 2
+const int expon = 20;
+const double f = 0.5; 
+TickType_t ticks = 100;
+
+size_t num;
+const int32_t amplitude = pow(2, expon); 
+int32_t sample = amplitude * -1; 
+int64_t t = 0;
+int64_t last_t = 0;
+const double T = 1000.0/f;
+const double m = (amplitude*2.0)/(T*1000.0);
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  t = esp_timer_get_time();       //double(esp_timer_get_time())/1000000.0;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  if (type == 0)
+  {
+    sample = amplitude * sin(2.0*M_PI*f*(t/1000000.0));
+  }
+  else if (type == 1)
+  {
+    sample = sin(2.0 * M_PI*f*(t/1000000.0))>=0.0 ? amplitude:-amplitude;
+  }
+  else if (type == 2)
+  {
+    if ((t - last_t) >= (T*1000))
+    {
+      last_t = t;
+      sample = amplitude * -1;
+    }
+    else
+    {
+      sample += m*(t - last_t);
+    }
+  }
+  
+  i2s_write(i2s_out, &sample, sizeof(sample), &num, ticks);
+  sample *=-1; 
+  i2s_write(i2s_out, &sample, sizeof(sample), &num, ticks);
+
+  Serial.println(sample);
+ }
